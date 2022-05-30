a17b5536147d0a4ce1a5a724a6993072292641ba
luismch <lh0202275202@gmail.com>
2022-05-30 10:47:50 -0500

    List comprehensions

--- curso-pyton/curso_python/list_comprehensions.py
+++ curso-pyton/curso_python/list_comprehensions.py
@@ -3,7 +3,7 @@ def run():
     # for i in range(1, 101):
     #     if i % 3 != 0:
     #         squares.append(i**2)
-    squares = [i**2 for i in range(1, 101) if i % 3 !=0]
+    squares = [i**2 for i in range(1, 101) if i % 3  !=0]
 
     print(squares)
 
