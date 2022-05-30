7e280a187f7c7010e9cc2fe0318845df2c06d39b
luismch <lh0202275202@gmail.com>
2022-05-30 11:34:53 -0500

    High order functions: filter, map y reduce

---
+++ High order functions__filter, map y reduce.py
@@ -0,0 +1,29 @@
+#uso de filter
+
+# my_list =[1,4,5,6,9,13,19,21]
+
+# odd = list(filter(lambda x:x % 2 !=0 , my_list))
+
+# print(odd)
+
+
+
+#uso de map 
+
+# my_list = [1,2,3,4,5]
+
+# squares = list(map(lambda x: x**2, my_list))
+
+# print(squares)
+
+
+
+#uso de reduce
+
+# from functools import reduce
+
+# my_list = [2,2,2,2,2]
+
+# all_multiplited = reduce(lambda a, b: a * b , my_list)
+
+# print(all_multiplited)
